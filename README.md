bebb6ffcd54c7322097eda78cdd73a274b446626
ALbert-Shen-code <siyuanshen1993@gmail.com>
2021-06-29 12:57:41 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# eommerce-project
+ 
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Products from './Components/Products/Products';
+
+function App() {
+  return (
+    <div className="App">
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
